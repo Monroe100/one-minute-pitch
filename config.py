d3fa4b9a33707e5fd88ceb030f5ea6f0b320ed10
Monroe100 <michelle1hunnid@gmail.com>
2018-05-21 12:28:15 +0300

    imported os in class Config in file config.py

--- config.py
+++ config.py
@@ -1,11 +1,25 @@
 
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+
+
 class ProdConfig(Config):
     '''
     Production configuration child class
     Args:
         Config: The parent configuration class with general configuration settings
     '''
-    pass
+    
 
 class DevConfig(Config):
     '''
