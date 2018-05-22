1479629e424d79f7aca624fd7781e84316b8ba53
Monroe100 <michelle1hunnid@gmail.com>
2018-05-22 11:52:15 +0300

    initialized bootstrap extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
-
+bootstrap = Bootstrap()
 
 
 def create_app(config_name):
     app = Flask(__name__)
 
-from .main import main as main_blueprint
-app.register_blueprint(main_blueprint)
+    #initializing flask extensions
+    bootstrap.init_app(app)
+    
 
-from .auth import auth as auth_blueprint
-app.register_blueprint(auth_blueprint)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
