50e83cdadc37f5ad075d117bf1ec110273422bc5
Monroe100 <michelle1hunnid@gmail.com>
2018-05-28 12:43:16 +0300

    made relevant changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 
 from flask import render_template,redirect,url_for,abort
 from . import main
-from ..models import Category, User,Peptalk, Comments
+from ..models import Category, User,Pitch, Comments
 from .. import db
 from flask_login import login_required, current_user
 from .forms import PitchForm,CommentForm
